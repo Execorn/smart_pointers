4d1ec33396e3f0931d503554261c5b5a6b38e8b6
execorn <n.biryukov.04@gmail.com>
2023-04-04 22:31:28 +0300

    starting writing unique_ptr

---
+++ include/unique_pointer.hpp
@@ -0,0 +1,21 @@
+#ifndef X17_UNIQUE_POINTER_STABLE
+#define X17_UNIQUE_POINTER_STABLE
+
+////////////////////////////////////////////////////////////
+/// Headers
+////////////////////////////////////////////////////////////
+#include <memory>
+#include <exception>
+#include <iostream>
+#include <cstdio>
+
+namespace X17 {
+
+template<typename T> 
+class unique_ptr {
+    constexpr unique_ptr
+};
+
+}
+
+#endif // !X17_UNIQUE_POINTER_STABLE
